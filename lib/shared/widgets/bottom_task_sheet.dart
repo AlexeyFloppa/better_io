db09c52fb60b5b9a495edca3737bf8bd679a1e69
AlexeyHeckerMode <AlexeyDevelopAcc@gmail.com>
2025-06-22 23:09:17 +0300

    feat: Move BottomTaskSheet, ColorPickerDialog, EditableListTile, and TextInputDialog components to improve readablillty

--- lib/shared/widgets/bottom_task_sheet.dart
+++ lib/shared/widgets/bottom_task_sheet.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 class BottomTaskSheet extends StatelessWidget {
   final VoidCallback onTaskEdit;
   final VoidCallback onTaskRemove;
+  final VoidCallback onTaskSubmit;
   final VoidCallback onRecurrencyRemove;
 
   const BottomTaskSheet({
     super.key,
     required this.onTaskEdit,
     required this.onTaskRemove,
+    required this.onTaskSubmit,
     required this.onRecurrencyRemove,
   });
 
@@ -20,10 +22,18 @@ class BottomTaskSheet extends StatelessWidget {
         children: [
           ListTile(
             leading: const Icon(Icons.edit),
-            title: const Text('Edit'),
+            title: const Text('Edit Task'),
             onTap: () {
               Navigator.pop(context);
               onTaskEdit();
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.check),
+            title: const Text('Submit Task'),
+            onTap: () {
+              Navigator.pop(context);
+              onTaskSubmit();
             },
           ),
           ListTile(
